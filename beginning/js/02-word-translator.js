488001948cbd001be5853407e735f8fc8fd6a64c
rosemaryto <rosemary_to@ymail.com>
2021-02-23 17:28:42 -0800

    Q2-complete

---
+++ beginning/js/02-word-translator.js
@@ -0,0 +1,13 @@
+var langPref = window.prompt("What language do you prefer? es, de, en, fr");
+
+if (langPref === "es") {
+    window.console.log("Hola mundo!");
+} else if (langPref === "de") {
+    window.console.log("Hallo welt!");
+} else if (langPref === "en") {
+    window.console.log("Hello world!");
+} else if (langPref === "fr") {
+    window.console.log("Bonjour le monde!");
+} else {
+    window.console.log("Hello world!");
+}
